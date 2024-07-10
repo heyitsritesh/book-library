b56f174201640829e3a6d2e12caddf080a00a17a
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-07-10 19:08:06 +0530

    Added function to add new books to the DOM

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const pages = document.querySelector('#pages');
 const newBookForm = document.querySelector('#newBookForm');
+const booksDatabase = document.querySelector('.booksDatabase > tbody');
 
 // Fetch Read Status From Radio Buttons
 function readStatus() {
@@ -42,6 +43,8 @@ function Book(title, author, pages, read) {
 newBookForm.addEventListener('submit', event => {
     event.preventDefault(); // Remove default form behaviour
     addBookToLibrary();
+    booksDatabase.textContent = '';
+    addToDatabase();
     newBookForm.reset();
 });
 
@@ -50,4 +53,19 @@ function addBookToLibrary() {
     let entry = new Book(title.value, author.value, pages.value, readStatus());
     myLibrary.push(entry);
     console.log(myLibrary);
+}
+
+// Function to Add Books from Array to Database
+function addToDatabase() {
+    myLibrary.forEach(book => {
+        let tr = document.createElement('tr');
+
+        for (let data in book) {
+            let td = document.createElement('td');
+            td.textContent = book[data];
+            tr.appendChild(td);
+        }
+
+        booksDatabase.appendChild(tr);
+    });
 }
